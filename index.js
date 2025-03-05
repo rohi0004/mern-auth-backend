693604a6b3e70170d4b24d29360e27a3da849beb
Rohit kumar <sahrohitkumar10@gmail.com>
2025-03-06 00:51:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const uri = process.env.URI
 const authRoutes = require('./routes/authRoutes')
 
 const allowedOrigins = [
+  'https://mern-auth-frontend-m65x.onrender.com/',
   'http://localhost:5173',
 ]
 
